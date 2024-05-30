3821568cc847305fdcde7b7568370737b32468e9
Khanh Do <khanhdo1219@gmail.com>
2024-05-30 16:31:51 +0700

    add pj

---
+++ app/layout.jsx
@@ -0,0 +1,28 @@
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & share AI promps",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <Provider>
+          <div className="name">
+            <div className="gradient" />
+            <main className="app">
+              <Nav />
+              {children}
+            </main>
+          </div>
+        </Provider>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
